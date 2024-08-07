bcba89347faa6dea4cdb4267dd7c31879de61b1a
Jack Hartwig <jackhartwig7@gmail.com>
2024-08-07 18:44:28 -0500

    made level editor inherit from game class

--- include/alchemy/levelEditor.h
+++ include/alchemy/levelEditor.h
@@ -8,45 +8,12 @@
 #include "player.h"
 #include <unordered_map>
 #include <alchemy/world.h>
+#include <alchemy/game.h>
 
-class LevelEditor {
+class LevelEditor : public Game{
 public:
-    LevelEditor();
-    ~LevelEditor();
-
-    void run();
 
 private:
-    void initGLFW();
-    void initGLEW();
-    void setupShaders();
-    void setupBuffers();
-    void processInput();
-    void update(double deltaTime);
-    void render();
-    void cleanup();
-    void checkCompileErrors(GLuint shader, std::string type);
-    void updateProjectionMatrix(int width, int height);
-
-    static void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-
-    GLFWwindow* window;
-    GLuint VAO, VBO;
-    GLuint shaderProgram, redShaderProgram;
-
-    NetworkManager networkManager;
-    Player clientPlayer;
-
-    int clientId;
-    double tickRate;
-    World world;
-
-    glm::mat4 projection;
-    std::unordered_map<int, Player> players;
-
-    static const char* vertexShaderSource;
-    static const char* fragmentShaderSource;
-    static const char* redFragmentShaderSource;
 };
 
 #endif // LEVELEDITOR_H
